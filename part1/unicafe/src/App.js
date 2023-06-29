d14ef6a9b30ef76111f14919bee6639f22bbe5b2
AnjaGberg <anja.gberg@gmx.de>
2023-06-29 09:19:05 +0200

    1.9-1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,15 +1,32 @@
 import { useState } from 'react'
 
 const Statistics = ({good, neutral, bad, total, average, positive}) => {
+  if (total === 0) {
+    return(
+      <p>No feedback given</p>
+    )
+  }
+
   return (
-    <>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {average}</p>
-      <p>positive {positive} %</p>
-    </>
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={total} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={positive} />
+      </tbody>
+    </table>
+  )
+}
+
+const StatisticLine = ({text, value}) => {
+  return (
+    <tr>
+      <td>{text} </td>
+      <td>{value}</td>
+    </tr> 
   )
 }
 
