24d72e2ba91410f9d941eb7f23330299124db894
Anja Greifenberg <anja.gberg@gmx.de>
2024-11-07 09:26:26 +0100

    2.3

--- part2/courseinformation/components/Total.jsx
+++ part2/courseinformation/components/Total.jsx
@@ -1,6 +1,6 @@
 
 const Total = ({parts}) => {
-  const total = parts.map(part => part.exercises).reduce((sum, num) => sum + num, 0)
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
   
   return (
     <div>
